1d1334cc52ec6ff49f66ccb250fe068083771c15
Marcel <roehrig.marcel@gmail.com>
2013-03-31 00:20:14 +0100

    kreis vergrößert sich (mit fehler)

--- elbtunnel.js
+++ elbtunnel.js
@@ -1,31 +1,43 @@
 //canvas-size
 c.width = 900;
 c.height = 450;
-
-d = function(e,f,g){
-                       if(t>e && t<f){
+d = function(g){
+                       if(8*t-25>g&&8*t-25<g+115){
                        a.beginPath();
                        a.arc(g,225,25,0,2*Math.PI);
                        a.fillStyle="white";
                        a.fill();
                        a.closePath();}};
 
+e = function(j,g,h){
+
+                      a.beginPath();  
+                       a.arc(g,h,j,0,2*Math.PI);
+                       a.fillStyle="white";
+                       a.fill();
+                       a.closePath();};
+
 t = 0;
 setInterval(function(){a.fillStyle="black";
-                       a.fillRect(0,0,900,450);
+                     
+                        a.fillRect(0,0,900,450);
                        t++;
                        a.closePath();
                        a.beginPath();
                        a.arc(8*t-25,225,25,0,2*Math.PI);
                        a.fillStyle="white";
-                       a.fill();
+                      if(t<90){ a.fill();
                        a.closePath();
-                       d(12,24,75);
-                       d(24,36,167);
-                       d(36,48,259);
-                       d(48,60,351);
-                       d(60,72,443);
-
-
-                       },40);
+                       d(115);
+                       d(230);
+                       d(345);
+                       d(460);
+                       d(575);
+                      d(690);}
+                     if(t>89&&t<107){
+                      e(25+3*(t-89),690,225);}
+                    if(t>106){
+                      e(76,690-(2*t+107),225);}
+console.log(g);
+},40);
 
